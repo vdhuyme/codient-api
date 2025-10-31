bb69df579814dd4dd94cb68b4a342e6856dca026
Vo Duc Huy <voduchuy2001@gmail.com>
2025-10-31 13:27:53 +0700

    chores

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,11 @@ import { errorHandler } from '@middlewares/error.handler'
 import cors from 'cors'
 import { notFound } from '@middlewares/not.found'
 import logger from '@config/logging'
-import { database } from 'data-source'
+import { database } from './data-source'
 import helmet from 'helmet'
 import compression from 'compression'
 import { InversifyExpressServer } from 'inversify-express-utils'
-import { container } from 'inversify-config'
+import { container } from './inversify-config'
 import { config } from '@config/app'
 
 async function bootstrap(): Promise<void> {
